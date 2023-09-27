3a35d8fdecb6100f81e984f690e74e9215ade1ba
Paula Cano <paulacanoroman@gmail.com>
2023-09-27 09:51:02 +0200

    feat: show list

--- todo-app-270923/components/TodoApp.jsx
+++ todo-app-270923/components/TodoApp.jsx
@@ -10,12 +10,23 @@ const TodoApp = () => {
         setTodoList([...todoList,todoText])
         setTodoText("")
     }
+
+    const showList = todoList.map((actualElement)=>{
+        return(
+            <>
+            <ul>
+                <li>{actualElement}</li>
+            </ul>
+            </>
+        )
+    })
     
     return (
     <>
     {todoList}
     <input type='text' onChange={(event)=>{setTodoText(event.target.value)}} value={todoText}></input>
     <button onClick={()=>{handleAdd()}}>enviar</button>
+    {showList}
     </>
   )
 }
