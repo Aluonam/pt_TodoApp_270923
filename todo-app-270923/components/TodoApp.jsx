536bee81cb03cc320e051beccc35585307f1f6e8
Paula Cano <paulacanoroman@gmail.com>
2023-09-27 09:48:47 +0200

    feat: input and button and addTask
    Agrega un campo de entrada de texto y un botón que permita al usuario agregar nuevas tareas a la lista.
    
    Implementa las funciones de manejo de eventos para agregar tareas. Cuando se agrega una tarea, debe aparecer en la lista.

--- todo-app-270923/components/TodoApp.jsx
+++ todo-app-270923/components/TodoApp.jsx
@@ -5,9 +5,17 @@ const TodoApp = () => {
     
     const [todoList, setTodoList] = useState([])
     const [todoText, setTodoText] = useState("")
-  return (
+
+    const handleAdd = ()=>{
+        setTodoList([...todoList,todoText])
+        setTodoText("")
+    }
+    
+    return (
     <>
     {todoList}
+    <input type='text' onChange={(event)=>{setTodoText(event.target.value)}} value={todoText}></input>
+    <button onClick={()=>{handleAdd()}}>enviar</button>
     </>
   )
 }
