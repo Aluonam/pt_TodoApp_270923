b8d3c6c3d4472343105b4320826ed3891ba11267
Paula Cano <paulacanoroman@gmail.com>
2023-09-27 09:30:03 +0200

    feat: new component and useStates

---
+++ todo-app-270923/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+
+const TodoApp = () => {
+
+    
+    const [todoList, setTodoList] = useState([])
+    const [todoText, setTodoText] = useState("")
+  return (
+    <div>TodoApp</div>
+  )
+}
+
+export default TodoApp
